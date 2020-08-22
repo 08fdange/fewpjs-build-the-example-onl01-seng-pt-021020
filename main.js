7411bbf5a3206380bce38514109878833556ae36
08fdange <08fdange@gmail.com>
2020-08-21 22:42:48 -0400

    Done.

--- main.js
+++ main.js
@@ -4,8 +4,36 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorElement = document.querySelector('#modal')
+const likes = document.querySelectorAll('.like-glyph')
+const errorMessage = document.querySelector('#modal-message')
 
+errorElement.classList.add('hidden');
 
+document.addEventListener('DOMContentLoaded', function(){
+  likes.forEach((like) => {
+    like.addEventListener('click', (event) => {
+      mimicServerCall()
+      .then(function() {
+        if (event.target.innerHTML == EMPTY_HEART) {
+          event.target.innerHTML = FULL_HEART;
+          event.target.classList.add('activated-heart')
+        }
+        else {
+          event.target.innerHTML = EMPTY_HEART;
+          event.target.classList.remove('activated-heart')
+        }
+      })
+      .catch((error) => {
+        setTimeout(function() {
+          errorMessage.innerHTML = `${error.message}`;
+          errorElement.classList.remove('hidden')
+        }, 2000)
+        errorElement.classList.add('hidden')
+      })
+    })
+  })
+})
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
